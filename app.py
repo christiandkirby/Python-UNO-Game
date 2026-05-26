53b31aba7f067c451b00de5e5436b137b9c70224
KingCK18 <christian.kirby2023@gmail.com>
2026-05-26 00:51:00 -0500

    Frontend Gameplay Layout : GUI Version 1.1

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask import render_template
 
+def card_to_filename(card):
+    return repr(card).lower().replace(' ', '_')
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    # player_hand = [card_to_filename(card) for card in players[0].hand]
+    set_of_cards = []
+    player_hand = ['red_2', 'blue_skip', 'wild', 'draw_four', 'yellow_reverse', 'green_draw_two']
+    return render_template('index.html', top_card ="red_2", player_hand=player_hand)
 
 
 if __name__ == "__main__":
